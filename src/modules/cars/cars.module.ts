5fa8851630bcde7e392b6589a39868165a00640b
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-08-13 19:12:10 +0300

    lessons/1---intro

---
+++ src/modules/cars/cars.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { CarsService } from "./cars.service";
+import { CarsController } from "./cars.controller";
+
+@Module({
+  imports: [],
+  controllers: [CarsController],
+  providers: [CarsService],
+  exports: [CarsService]
+})
+export class CarsModule {
+}
