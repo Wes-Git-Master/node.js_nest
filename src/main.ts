2e2737f1f379a0ff70ae1edf6b9608ea0d0a6e7a
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-08-14 00:43:49 +0300

    lessons/2---swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Wes app')
+    .setDescription('The API description')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      docExpansion: 'list',
+      persistAuthorization: true,
+      defaultModelsExpandDepth: 7,
+    },
+  });
+  const port = 3000;
+  const host = 'localhost';
+  await app.listen(port, () => {
+    Logger.log(`server running on http://${host}:${port}`);
+    Logger.log(`Swagger running on http://${host}:${port}/docs`);
+  });
 }
-bootstrap();
+
+void bootstrap();
