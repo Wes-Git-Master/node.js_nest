5b249615bed0d60be09f250f02376f5836e2363e
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-08-21 17:45:39 +0300

    lessons/6---exception-filters_sentry

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
+import { LoggerService } from '../logger/logger.service';
 import { PostsService } from '../posts/posts.service';
-import { CreateUserDto } from './dto/req/create-user.dto';
-import { UpdateUserDto } from './dto/req/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly carsService: PostsService) {}
+  constructor(
+    private readonly carsService: PostsService,
+    private readonly logger: LoggerService,
+  ) {}
 
-  public async create(createUserDto: CreateUserDto): Promise<any> {
+  public async create(): Promise<any> {
     this.carsService.create({});
+    this.logger.log('This is a test message');
+    throw new Error('This is a test error');
     return 'This action adds a new user';
   }
 
@@ -21,10 +25,7 @@ export class UsersService {
     return `This action returns a #${id} user`;
   }
 
-  public async updateMe(
-    id: number,
-    updateUserDto: UpdateUserDto,
-  ): Promise<any> {
+  public async updateMe(id: number): Promise<any> {
     return `This action updates a #${id} user`;
   }
 
